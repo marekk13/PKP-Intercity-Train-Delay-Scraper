d2a95b6777f36866b3715f5652b274881e645967
marekk13 <mar.kostrz@onet.pl>
2025-10-14 18:41:17 +0200

    Add user-agent

--- get_delays.py
+++ get_delays.py
@@ -1,6 +1,7 @@
 import re
 import time
 import logging
+import random
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -241,6 +242,13 @@ def get_delays(trains_data: list = None, logger=None) -> list:
     opts.add_argument("--disable-gpu")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
+
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+    opts.add_argument(f'user-agent={user_agent}')
+
+    opts.add_argument('--disable-blink-features=AutomationControlled')
+    opts.add_experimental_option("excludeSwitches", ["enable-automation"])
+    opts.add_experimental_option('useAutomationExtension', False)
 
     try:
         driver = webdriver.Remote(
